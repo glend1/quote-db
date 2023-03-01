2eb4b2e90354d88303d21869f922ed51336f855e
glen <glen_dovey@hotmail.com>
2023-03-01 17:23:31 +0000

    testing pk

--- pages/api/authors.js
+++ pages/api/authors.js
@@ -3,10 +3,11 @@ import { connectToDatabase } from "../../util/mongodb";
 export default async (req, res) => {
   const { db } = await connectToDatabase();
   const data = await db.collection("collection").aggregate([
-    {"$group" : {_id:"$author", count:{$sum:1}}},
-    {$project: { _id: 0, author: "$_id", count: 1} }
-  ]).sort({"author": 1}).toArray();
+    { "$group": { _id: "$author", count: { $sum: 1 } } },
+    { $project: { _id: 0, author: "$_id", count: 1 } }
+  ]).sort({ "author": 1 }).toArray();
   res.statusCode = 200;
   res.setHeader('Content-Type', 'application/json');
   res.end(JSON.stringify(data));
+  console.log(process.env.FIREBASE_KEY)
 };
